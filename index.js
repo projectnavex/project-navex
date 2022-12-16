1b9989f8fd33dd92dc235cc0cc6aa1979ac6a982
Wuihee <wuihee@gmail.com>
2022-12-16 17:02:57 +0800

    Updated js and css files.

---
+++ index.js
@@ -0,0 +1,17 @@
+function initMap() {
+    var options = {
+        zoom: 15,
+        center: {lat: 1.412811, lng: 103.774780}
+    }
+    var map = new google.maps.Map(document.getElementById('map'), options)
+    map.addListener("click", function(e) {
+        addLatLng(e.latLng, map);
+    });
+}
+
+function addLatLng(position, map) {
+    new google.maps.Marker({
+        position: position,
+        map: map,
+    })
+}
