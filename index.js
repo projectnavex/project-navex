274ce251f826f66f6d9fd2006834fbfe376bc70b
Wuihee <wuihee@gmail.com>
2023-01-16 14:34:08 +0800

    Updated marker infowindow

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let map, poly, infowindow, Popup;
+let map, poly, infowindow, activeMarker;
 let uniqueID = 1;
 let markers = [];
 const TRAINING_AREAS = {
@@ -196,67 +196,22 @@ function initMap() {
     // Initialize polylines onto maps.
     poly.setMap(map);
 
+    // Initialize infowindow for each marker.
+    const content = 
+		'<div id="infowindow">' + 
+		'<input type="button" value="Delete" onclick="deleteMarker()"/>' +
+		'</div>';
+    infowindow = new google.maps.InfoWindow({content: content})
+
     // Add markers to map upon user click.
     map.addListener("click", e => addLatLng(e.latLng, map))
-
-    // Initialize class for infowindows for markers.
-    class Popup extends google.maps.OverlayView {
-        position;
-        containerDiv;
-        constructor(position, content) {
-            super();
-            this.position = position;
-            content.classList.add("popup-bubble");
-    
-            // This zero-height div is positioned at the bottom of the bubble.
-            const bubbleAnchor = document.createElement("div");
-        
-            bubbleAnchor.classList.add("popup-bubble-anchor");
-            bubbleAnchor.appendChild(content);
-            // This zero-height div is positioned at the bottom of the tip.
-            this.containerDiv = document.createElement("div");
-            this.containerDiv.classList.add("popup-container");
-            this.containerDiv.appendChild(bubbleAnchor);
-            // Optionally stop clicks, etc., from bubbling up to the map.
-            Popup.preventMapHitsAndGesturesFrom(this.containerDiv);
-        }
-        /** Called when the popup is added to the map. */
-        onAdd() {
-            this.getPanes().floatPane.appendChild(this.containerDiv);
-        }
-        /** Called when the popup is removed from the map. */
-        onRemove() {
-            if (this.containerDiv.parentElement) {
-                this.containerDiv.parentElement.removeChild(this.containerDiv);
-            }
-        }
-        /** Called each frame when the popup needs to draw itself. */
-        draw() {
-            const divPosition = this.getProjection().fromLatLngToDivPixel(this.position);
-            // Hide the popup when it is far out of view.
-            const display =
-                Math.abs(divPosition.x) < 4000 && Math.abs(divPosition.y) < 4000
-                ? "block"
-                : "none";
-        
-            if (display === "block") {
-                this.containerDiv.style.left = divPosition.x + "px";
-                this.containerDiv.style.top = divPosition.y + "px";
-            }
-        
-            if (this.containerDiv.style.display !== display) {
-                this.containerDiv.style.display = display;
-            }
-        }
-    }
-    infowindow = new Popup(new google.maps.LatLng(1.412811, 103.774780), document.getElementById("content"));
 }
 
 function addLatLng(position, map) {
     // Add marker to map.
     const marker = new google.maps.Marker({
         position: position,
-        draggable: true,
+        draggable: false,
         map: map,
         animation: google.maps.Animation.DROP
     })
@@ -265,19 +220,10 @@ function addLatLng(position, map) {
     marker.id = uniqueID;
     uniqueID++;
 
-    // Initialize infowindow for each marker.
-    /*
-    const content = 
-		'<div id="infowindow">' + 
-		'<input type="button" value="Delete" onclick="deleteMarker('+ marker.id +')"/>' +
-		'</div>';
-    const infowindow = new google.maps.InfoWindow({content: content}) */
-
     // Add even listener to markers.
     marker.addListener("click", () => {
-        const infowindowPos = new google.map.LatLng(marker.anchorPoint.y, marker.anchorPoint.y);
-        infowindow.position = infowindowPos;
-        infowindow.setMap(map);
+        infowindow.open(map, marker);
+        activeMarker = marker.id;
     })
 
     /*
@@ -296,16 +242,16 @@ function addLatLng(position, map) {
 }
 
 // Delete specific marker.
-function deleteMarker(id) {
+function deleteMarker() {
   	for (var i = 0; i < markers.length; i++) {
-    	if (markers[i].id == id) {
+    	if (markers[i].id == activeMarker) {
       		// Remove the marker from the map.
 			markers[i].setMap(null);
 
 			// Remove the marker from the markers array.
 			markers.splice(i, 1);
+        }
     }
-  }
   
 	// Refresh polyline path so that it no longer follows deleted marker.
 	poly.setPath(markers.map(marker => marker.position));
